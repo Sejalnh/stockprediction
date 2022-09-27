c931abf80655a62e761befbf4e2c86f5c94e7003
Sejal Nathu-Hari <snathuhari@yahoo.com>
2022-09-27 17:57:59 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from  prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+
+START = "2015-01-01" # Start date (Get info from this date)
+TODAY = date.today().strftime("%Y-%m-%d") # (Get info till today)
+
+st.title("Stock Prediction App") # title
+
+stocks = ("AAPL", "GOOG", "MSFT", "GME") # Stock options
+selected_stock = st.selectbox("Select dataset for prediction", stocks) # Allows you to select a specific stock
+
+n_years = st.slider("Years of prediction:", 1, 4) # Adds a slider to select the num of years for predicition
+period = n_years * 365 
+
+@st.cache
+def load_data(ticker): # ticker : Stock Name
+    data = yf.download(ticker, START, TODAY) # downloads data from start date to today
+    data.reset_index(inplace = True) # puts date in the first column
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stock) 
+data_load_state.text("Loading data... done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+    
+plot_raw_data()  
+
+# Forecasting
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('Forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('Forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
